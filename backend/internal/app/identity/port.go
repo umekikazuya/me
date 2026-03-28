a0e36744091a7424331b95ebf2a86c8c29a3932b
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-03-28 20:58:56 +0900

    feat: TokenService起因のエラーを定義

--- backend/internal/app/identity/port.go
+++ backend/internal/app/identity/port.go
@@ -2,14 +2,21 @@ package identity
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	domain "github.com/umekikazuya/me/internal/domain/identity"
 )
 
+var (
+	ErrTokenInvalid = errors.New("token is invalid")
+	ErrTokenExpired = errors.New("token is expired")
+	ErrTokenRevoked = errors.New("token is revoked")
+)
+
 type TokenService interface {
 	// アカウント情報から新しいアクセストークンを生成
-	Generate(ctx context.Context, account *domain.Identity, expiresIn time.Duration) (string, error)
+	Generate(ctx context.Context, identity domain.Identity, expiresIn time.Duration) (string, error)
 
 	// トークンのハッシュ化を行う
 	Hash(ctx context.Context, token string) (string, error)
