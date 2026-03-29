994820ded2c6baf8152ab90edca2606cf965d510
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-03-29 10:29:40 +0900

    feat: GenerateAT, Validate, Hash のロジックを実装

--- backend/internal/infra/token/token.go
+++ backend/internal/infra/token/token.go
@@ -2,13 +2,19 @@ package token
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"time"
 
-	appidentity "github.com/umekikazuya/me/internal/app/identity"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	app "github.com/umekikazuya/me/internal/app/identity"
 	domain "github.com/umekikazuya/me/internal/domain/identity"
+	"github.com/umekikazuya/me/pkg/errs"
 )
 
-var _ appidentity.TokenService = (*JWTTokenService)(nil)
+var _ app.TokenService = (*JWTTokenService)(nil)
 
 type JWTTokenService struct {
 	secret   []byte
@@ -22,18 +28,76 @@ func NewJWTTokenService(secret string, atExpiry time.Duration) *JWTTokenService 
 	}
 }
 
-func (s *JWTTokenService) GenerateAT(_ context.Context, _ domain.Identity) (string, error) {
+func (s *JWTTokenService) GenerateAT(ctx context.Context, identity domain.Identity) (string, error) {
+	now := time.Now().UTC()
+	expiresAt := now.Add(time.Minute * 15)
+
+	claims := jwt.RegisteredClaims{
+		Subject:   identity.ID(),
+		IssuedAt:  jwt.NewNumericDate(now),
+		ExpiresAt: jwt.NewNumericDate(expiresAt),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString(s.secret)
+	if err != nil {
+		return "", app.ErrTokenInvalid
+	}
+
+	return signedToken, nil
+}
+
+func (s *JWTTokenService) GenerateRT(ctx context.Context) (string, error) {
 	return "", nil
 }
 
-func (s *JWTTokenService) GenerateRT(_ context.Context) (string, error) {
-	return "", nil
+func (s *JWTTokenService) Hash(
+	ctx context.Context,
+	input string,
+) (string, error) {
+	if input == "" {
+		return "", fmt.Errorf(
+			"Hash: 入力文字が空です %w", errs.ErrInternal,
+		)
+	}
+	hashed := sha256.Sum256(
+		[]byte(input),
+	)
+
+	return hex.EncodeToString(hashed[:]), nil
 }
 
-func (s *JWTTokenService) Hash(_ context.Context, _ string) (string, error) {
-	return "", nil
-}
+func (s *JWTTokenService) Validate(ctx context.Context, token string) error {
+	// トークンの長さ制限を追加
+	if len(token) > 4096 {
+		return app.ErrTokenInvalid
+	}
+	validatedToken, err := jwt.ParseWithClaims(
+		token, &jwt.RegisteredClaims{},
+		func(token *jwt.Token) (any, error) {
+			if token.Method != jwt.SigningMethodHS256 {
+				return nil, app.ErrTokenInvalid
+			}
+			return s.secret, nil
+		},
+	)
+	if err != nil {
+		return app.ErrTokenInvalid
+	}
 
-func (s *JWTTokenService) Validate(_ context.Context, _ string) error {
+	if !validatedToken.Valid {
+		return app.ErrTokenInvalid
+	}
+
+	claims, ok := validatedToken.Claims.(*jwt.RegisteredClaims)
+	if !ok {
+		return app.ErrTokenInvalid
+	}
+
+	_, err = uuid.Parse(claims.Subject)
+	if err != nil {
+		return app.ErrTokenInvalid
+	}
+
 	return nil
 }
+
