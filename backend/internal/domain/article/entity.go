2dc779359ba9afe42675116e769a3f7fc7c22656
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-04-13 03:15:19 +0900

    feat: Article構造体のフィールド定義

--- backend/internal/domain/article/entity.go
+++ backend/internal/domain/article/entity.go
@@ -1,3 +1,14 @@
 package article
 
-type Article struct {}
+import "time"
+
+type Article struct {
+	id               id
+	title            title
+	platform         platform
+	publishedAt      publishedAt
+	articleUpdatedAt articleUpdatedAt
+	isActive         isActive
+	createdAt        time.Time
+	updatedAt        time.Time
+}
