40eaebccaea731318c150f3ef4c849c37c1801e9
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-04-13 03:41:48 +0900

    feat: 振る舞いのスタブ実装

--- backend/internal/domain/article/entity.go
+++ backend/internal/domain/article/entity.go
@@ -1,7 +1,13 @@
 package article
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
+// 型定義
+
+// Article はArticle集約を表現
 type Article struct {
 	id               id
 	title            title
@@ -11,4 +17,101 @@ type Article struct {
 	isActive         isActive
 	createdAt        time.Time
 	updatedAt        time.Time
+}
+
+// Opt はFOを表現
+type Opt func(*Article) error
+
+// ファクトリー関数
+
+// newArticle はArticle集約のファクトリー関数
+func newArticle(
+	inputID, inputTitle, inputPlatform string,
+	inputIsAcrive bool,
+	opts ...Opt,
+) (*Article, error) {
+	id, err := newID(inputID)
+	if err != nil {
+		return nil, err
+	}
+	title, err := newTitle(inputTitle)
+	if err != nil {
+		return nil, err
+	}
+	platform, err := newPlatform(inputPlatform)
+	if err != nil {
+		return nil, err
+	}
+	isActive, err := newIsActive(inputIsAcrive)
+	if err != nil {
+		return nil, err
+	}
+	e := Article{
+		id:       id,
+		title:    title,
+		platform: platform,
+		isActive: isActive,
+	}
+	for _, opt := range opts {
+		if opt == nil {
+			return nil, errors.New("nil option is not allowed")
+		}
+		if err := opt(&e); err != nil {
+			return nil, err
+		}
+	}
+	return nil, nil
+}
+
+// --- 振る舞い ---
+
+// Index は記事のインデックス登録を実施
+
+func Index(
+	inputID, inputTitle, inputPlatform string,
+	inputIsAcrive bool,
+	opts ...Opt,
+) (*Article, error) {
+	return nil, nil
+}
+
+// Register は記事の手動登録を実施
+func Register(
+	inputID, inputTitle, inputPlatform string,
+	inputIsAcrive bool,
+	opts ...Opt,
+) (*Article, error) {
+	return nil, nil
+}
+
+// Reindex はインデクサーによる全上書き更新を実施
+//
+// id, platformの上書きは不可
+func (e *Article) Reindex(
+	inputID, inputTitle, inputPlatform string,
+	inputIsAcrive bool,
+	opts ...Opt,
+) error {
+	return nil
+}
+
+// Update は手動上書き更新
+//
+// id, platformの上書きは不可
+func (e *Article) Update(
+	inputID, inputTitle, inputPlatform string,
+	inputIsAcrive bool,
+	opts ...Opt,
+) error {
+	return nil
+}
+
+// Deactivate はインデクサーによる論理削除
+func (e *Article) Deactivate() error {
+	return nil
+}
+
+// Remove は手動論理削除
+func (e *Article) Remove() error {
+	return nil
 }
